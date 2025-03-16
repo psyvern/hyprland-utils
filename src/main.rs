03b14775bd996d6c3c1a16382f32fbb7a416f78d
psyvern <psyv3rn@proton.me>
2025-03-16 10:19:24 +0100

    Better window handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::fmt::Display;
+use std::{fmt::Display, io::Write, path::Path, process::Stdio};
 
 use chrono::Local;
 use clap::{Parser, Subcommand, ValueEnum};
 use hyprland::{
-    data::{Client, CursorPosition, Monitor},
+    data::{Client, Clients, CursorPosition, Monitor, Workspace},
     dispatch::{Dispatch, DispatchType, Position},
     shared::{HyprData, HyprDataActive, HyprDataActiveOptional},
     Result as HResult,
 };
+use itertools::Itertools;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -74,6 +75,56 @@ fn toggle_float() -> HResult<()> {
     Ok(())
 }
 
+fn grab_window(path: &Path) -> HResult<bool> {
+    let workspace = Workspace::get_active()?;
+    let clients = Clients::get()?
+        .into_iter()
+        .filter(|x| x.workspace.id == workspace.id)
+        .map(|x| format!("{},{} {}x{}", x.at.0, x.at.1, x.size.0, x.size.1))
+        .join("\n");
+    let mut child = std::process::Command::new("slurp")
+        .arg("-w")
+        .arg("0")
+        .arg("-f")
+        .arg("%x,%y %wx%h")
+        .arg("-r")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let mut stdin = child.stdin.take().expect("Failed to open stdin");
+    std::thread::spawn(move || {
+        stdin
+            .write_all(clients.as_bytes())
+            .expect("Failed to write to stdin");
+    });
+    let output = child
+        .wait_with_output()
+        .expect("Failed to read stdout")
+        .stdout;
+
+    if output.is_empty() {
+        Ok(false)
+    } else {
+        save_geometry(path, output).map(|_| true)
+    }
+}
+
+fn save_geometry(path: &Path, geometry: Vec<u8>) -> HResult<()> {
+    std::process::Command::new("grim")
+        .arg("-g")
+        .arg(String::from_utf8(geometry).unwrap())
+        .arg(path)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+    // wl-copy --type image/png < "$output"
+
+    Ok(())
+}
+
 fn screenshot(mode: ScreenshotMode) -> HResult<()> {
     let file = Local::now().format("%Y-%m-%d_%H-%M-%S.png").to_string();
     let directory = homedir::my_home()
@@ -85,24 +136,42 @@ fn screenshot(mode: ScreenshotMode) -> HResult<()> {
 
     std::fs::create_dir_all(&directory).unwrap();
 
-    let mut output = std::process::Command::new("hyprshot");
-    let output = output.arg("-m");
-    let output = match mode {
-        ScreenshotMode::Point => output.arg("point"),
-        ScreenshotMode::Region => output.arg("region"),
-        ScreenshotMode::Window => output.arg("window"),
-        ScreenshotMode::Display => output.arg("active").arg("-m").arg("output"),
-    };
-    let output = output
-        .arg("-o")
-        .arg(&directory)
-        .arg("-f")
-        .arg(&file)
-        .arg("-s")
-        .output()
-        .unwrap();
+    let result = match mode {
+        ScreenshotMode::Window => {
+            hyprland::keyword::Keyword::set("general:col.inactive_border", 0xFFFFFFFFu32)?;
+            hyprland::keyword::Keyword::set("general:col.active_border", 0xFFFFFFFFu32)?;
+            hyprland::keyword::Keyword::set("decoration:rounding", 0)?;
+            hyprland::keyword::Keyword::set("decoration:dim_inactive", 0)?;
+            hyprland::dispatch!(Custom, "submap", "empty")?;
+            let result = grab_window(&path)?;
+            hyprland::ctl::reload::call()?;
+            hyprland::dispatch!(Custom, "submap", "reset")?;
 
-    if output.stderr.is_empty() {
+            result
+        }
+        _ => {
+            let mut output = std::process::Command::new("hyprshot");
+            let output = output.arg("-m");
+            let output = match mode {
+                ScreenshotMode::Point => output.arg("point"),
+                ScreenshotMode::Region => output.arg("region"),
+                ScreenshotMode::Window => unreachable!("Window already handled"),
+                ScreenshotMode::Display => output.arg("active").arg("-m").arg("output"),
+            };
+            let output = output
+                .arg("-o")
+                .arg(&directory)
+                .arg("-f")
+                .arg(&file)
+                .arg("-s")
+                .output()
+                .unwrap();
+
+            output.stderr.is_empty()
+        }
+    };
+
+    if result {
         use notify_rust::Notification;
         Notification::new()
             .image_path(&path.to_string_lossy())

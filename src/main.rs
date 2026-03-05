13ea38025858f5891908bbb0e930cedfdde9cba3
psyvern <psyv3rn@proton.me>
2026-03-05 10:54:34 +0100

    new-terminal: temporarily disable single instance

--- src/main.rs
+++ src/main.rs
@@ -379,7 +379,7 @@ fn new_terminal() -> HResult<()> {
         };
 
         let error = exec::Command::new("ghostty")
-            .arg("--gtk-single-instance=true")
+            // .arg("--gtk-single-instance=true")
             .arg(format!("--working-directory={}", path.to_string_lossy()))
             .exec();
 
